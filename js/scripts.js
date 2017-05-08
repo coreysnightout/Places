d8868b1c8a4705aef26c49c0ae488331045b11ea
Brendan Hanna and Corey Redding <brendan@>
2017-05-08 11:33:02 -0700

    change js so that the output being displayed is a property of an object created in js, rather than just a stored string from the input

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
+//back end
+function Place(location, time, things) {
+  this.poop = location;
+  this.timeOfYear = time;
+  this.thingsToDo = things;
+}
+
+
+
 $(document).ready(function() {
   $("form#new-places").submit(function(event) {
     event.preventDefault();
 
     var inputtedLocation =$("input#new-location").val();
+    var inputtedTime =$("input#new-date").val();
+    var inputtedThings =$("input#new-things").val();
 
-    $("ul#places").append("<li><span class='location'>" + inputtedLocation + "</span></li>");
+
+    var newPlace = new Place(inputtedLocation, inputtedTime, inputtedThings);
+
+    $("ul#places").append("<li><span class='location'>" + newPlace.poop + "</span></li>");
 
     $(".location").last().click(function() {
       $("#show-location").show();
-      $("#show-location h2").text(inputtedLocation);
+      $("#show-location h2").text(newPlace.poop);
     });
 
   });
