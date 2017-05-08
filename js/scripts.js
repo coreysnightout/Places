04a51cd25b61402b46875311086330a0afe1e3a2
Brendan Hanna and Corey Redding <brendan@>
2017-05-08 11:08:34 -0700

    add js click function so that when user clicks location, program will display location in div that is shown upon click.  however, the location displayed is not yet a property of an object created with constructors, but simply the variable in which the user input for location was stored

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
 
     var inputtedLocation =$("input#new-location").val();
 
-    $("ul#places").append(inputtedLocation);
+    $("ul#places").append("<li><span class='location'>" + inputtedLocation + "</span></li>");
+
+    $(".location").last().click(function() {
+      $("#show-location").show();
+      $("#show-location h2").text(inputtedLocation);
+    });
 
   });
 });
